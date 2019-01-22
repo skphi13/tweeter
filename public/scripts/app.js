8ce3720bda953340651e3bc4831de2e4f498cd1c
skphi13 <pphang804@gmail.com>
2019-01-22 23:51:31 +0000

    added created by date and post ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,6 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 // Fake data taken from tweets.json
+$(document).ready(function() {
 const data = [
   {
     "user": {
@@ -51,6 +52,32 @@ const data = [
   }
 ];
 
+function createDate(date) {
+  var seconds = Math.floor((new Date() - date) / 1000);
+  var interval = Math.floor(seconds / 31536000);
+
+  if (interval > 1) {
+    return interval + " years";
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return interval + " months";
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return interval + " days";
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return interval + " hours";
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return interval + " minutes";
+  }
+  return Math.floor(seconds) + " seconds";
+};
+
 function renderTweets(tweets) {
   // loops through tweets
     // calls createTweetElement for each tweet
@@ -63,16 +90,39 @@ function renderTweets(tweets) {
 
 function createTweetElement(tweet) {
     console.log(tweet);
+    // let date = new Date(tweet.created_at).toString().slice(0, 15);
     let imgUrl = $('<img>').attr("src", tweet.user.avatars.large);
     let h1 = $('<h1>').text(tweet.user.name);
     let span = $('<span>').text(tweet.user.handle);
     let header = $('<header>').append(imgUrl).append(h1).append(span);
     let div = $('<div>').text(tweet.content.text);
-    let footer = $('<footer>').append('<span>');
+    let footer = $('<footer>').append('<span>').text("Created " + createDate(tweet.created_at));
     let $tweet = $('<article>').addClass('tweet').append(header).append(div).append(footer);
+    
   // ...
   return $tweet;
 }
+
+$(function () {
+  let $post = $('form input');
+  $post.click(function() {
+    let tweetData = $('form').serialize();
+    event.preventDefault();
+    console.log("submit buttom click");
+    $.ajax({
+      url: "/tweets/",
+      type: "POST",
+      data: tweetData,
+      success: function(newTweets) {
+      console.log('Success: ', newTweets);
+      }
+    });
+  });
+});
+
 renderTweets(data);
+});
+
+
 
 
